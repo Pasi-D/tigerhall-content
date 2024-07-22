ff2b60dec35f1fb44a80101048e2a2321334fc60
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-07-22 23:38:35 +0530

    Add PP Neuman fonts

---
+++ src/app/components/theme/fonts.ts
@@ -0,0 +1,41 @@
+import localFont from "next/font/local";
+
+const ppNeuman = localFont({
+  src: [
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-SemiBolditalic.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-Medium.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-Thin.woff2",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "../../../../public/fonts/PPNeueMontreal-Book.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pp",
+});
+
+export const fonts = {
+  ppNeuman,
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { fonts } from "./components/theme/fonts";
+import ChakraProvider from "./components/providers/chakra";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={fonts.ppNeuman.className}>
+        <ChakraProvider>{children}</ChakraProvider>
+      </body>
     </html>
   );
 }
