20fdba4577b2370c42b1de9b3a4e82928dbafe1a
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-07-23 18:14:34 +0530

    GQL query to fetch podcasts

---
+++ src/app/lib/graphql/queries.ts
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const GET_PODCASTS_QUERY = gql`
+  query GetPodcasts($limit: Int!, $keywords: String!) {
+    contentCards(filter: { limit: $limit, keywords: $keywords, types: [PODCAST] }) {
+      edges {
+        ... on Podcast {
+          id
+          name
+          image {
+            ...Image
+          }
+          categories {
+            ...Category
+          }
+          experts {
+            ...Expert
+          }
+        }
+      }
+    }
+  }
+
+  fragment Image on Image {
+    uri
+  }
+
+  fragment Category on Category {
+    id
+    name
+  }
+
+  fragment Expert on Expert {
+    firstName
+    lastName
+    title
+    company
+  }
+`;
