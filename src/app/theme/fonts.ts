2072b584bad18c73c36352049a402a16f524da3b
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-07-23 18:13:57 +0530

    Fix fonts imports

--- src/app/theme/fonts.ts
+++ src/app/theme/fonts.ts
@@ -3,32 +3,32 @@ import localFont from "next/font/local";
 const ppNeuman = localFont({
   src: [
     {
-      path: "../../../../public/fonts/PPNeueMontreal-Bold.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../../../../public/fonts/PPNeueMontreal-SemiBolditalic.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-SemiBolditalic.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../../../../public/fonts/PPNeueMontreal-Medium.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-Medium.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../../../../public/fonts/PPNeueMontreal-Italic.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-Italic.woff2",
       weight: "400",
       style: "italic",
     },
     {
-      path: "../../../../public/fonts/PPNeueMontreal-Thin.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-Thin.woff2",
       weight: "300",
       style: "italic",
     },
     {
-      path: "../../../../public/fonts/PPNeueMontreal-Book.woff2",
+      path: "../../../public/fonts/PPNeueMontreal-Book.woff2",
       weight: "400",
       style: "normal",
     },
