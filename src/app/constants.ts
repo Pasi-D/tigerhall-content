6b140620cbb32a50b3abd84a5ac435b044c3afe9
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-07-24 21:11:21 +0530

    Fixed dimensions of images

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -5,8 +5,8 @@ export const PODCAST_GRID_COLUMNS: SimpleGridProps["columns"] = [1, null, 3];
 export const ITEMS_PER_PAGE = 10;
 
 export const PODCAST_IMAGE_ATTRIBUTES = {
-  IMAGE_WIDTH: 244,
-  IMAGE_HEIGHT: 100,
+  IMAGE_WIDTH: 400,
+  IMAGE_HEIGHT: 150,
 };
 
 export const SEARCH_DEBOUNCE_THRESHOLD = 300;
