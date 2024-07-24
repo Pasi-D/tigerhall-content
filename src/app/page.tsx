b516681f4dc4e3eeb45671b7a64a5802127cabd8
Pasindu Dilshan <pasindu_dilshan@outlook.com>
2024-07-24 11:54:16 +0530

    Chore: Update comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,13 @@ import { PreloadQuery } from "./lib/client";
 import { GET_PODCASTS_QUERY } from "./lib/graphql/queries";
 import { GetPodcastsVariables, Podcasts } from "./types";
 
+// https://github.com/apollographql/apollo-client-nextjs/issues/332
 const Home: React.FC = async () => {
   return (
     <main className="flex min-h-screen items-center flex-col font-pp md:p-24 p-8">
       <PreloadQuery query={GET_PODCASTS_QUERY} variables={{ limit: 20, keywords: "", offset: 0 }}>
         {queryRef => (
           <Suspense fallback={<>loading</>}>
-            {/* https://github.com/apollographql/apollo-client-nextjs/issues/332 */}
             <PodcastList
               queryRef={queryRef as TransportedQueryRef<Podcasts, GetPodcastsVariables>}
             />
